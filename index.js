29e58493bc8ca17a86a7f72cde950935a861f1a8
4cupscafe <107203896+4cupscafe@users.noreply.github.com>
2022-06-29 00:18:19 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+var tog = document.getElementById("tog");
+var education = document.getElementById("education");
+tog.addEventListener("click",function() {
+  education.classList.toggle("open");
+})
