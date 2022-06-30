34b5204fe5389cc5a2b6106560978d08f20ad570
4cupscafe <107203896+4cupscafe@users.noreply.github.com>
2022-06-30 03:34:18 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ var education = document.getElementById("education");
 tog.addEventListener("click",function() {
   education.classList.toggle("open");
 })
+
+var achiev = document.getElementById("achiev");
+var achievments = document.getElementById("achievments");
+achiev.addEventListener("click",function() {
+  achievments.classList.toggle("open");
+})
